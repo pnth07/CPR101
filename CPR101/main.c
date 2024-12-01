9c0a4ec399e76c3d9da8e834a68be01e4c4573fd
pnth07 <phannhatthinh99@gmail.com>
2024-12-01 14:24:25 -0500

    Initial Commit

---
+++ CPR101/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CPR101
+//
+//  Created by Thinh on 2024-12-01.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
